b41d7630d05b26d8015a0b9168e621b1b5715105
Gordon Ng <58711049+DrNogNog@users.noreply.github.com>
2022-06-23 14:20:39 -0400

    Added collapsable #regions.

--- BluetoothClass.cs
+++ BluetoothClass.cs
@@ -1,9 +1,13 @@
 using System;
+using Windows.Devices.Bluetooth.Advertisement;
 
 namespace Blueberry.Desktop.WindowsApp.Bluetooth
 {
     public class BluetoothClass
     {
-        
+        /// Wrap class and uses packaged Bluetooth
+        public BluetoothClass(){
+            
+        }
     }
 }
