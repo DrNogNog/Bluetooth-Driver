7ab048ba285561eb9000b5b117da94909d73b2d5
Gordon Ng <58711049+DrNogNog@users.noreply.github.com>
2022-06-23 14:20:55 -0400

    Added Collapsable #regions and #endregions.

--- BluetoothClass.cs
+++ BluetoothClass.cs
@@ -5,9 +5,23 @@ namespace Blueberry.Desktop.WindowsApp.Bluetooth
 {
     public class BluetoothClass
     {
+        // private region
+        #region Private Members
+        
+        private BluetoothClass newWatcher;
+        
+        #endregion
+        
+
+        
+        #region Constructor
+
+
         /// Wrap class and uses packaged Bluetooth
         public BluetoothClass(){
+            newWatcher = new BluetoothClass
             
         }
+        #endregion
     }
 }
